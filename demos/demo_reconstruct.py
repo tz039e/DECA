c2b3501cbc853e4025ec9f845a2bc8d487a328dd
tz039e <tz039e@gmail.com>
2022-07-27 15:04:23 +0800

    save pose

--- demos/demo_reconstruct.py
+++ demos/demo_reconstruct.py
@@ -56,7 +56,8 @@ def main(args):
                 tform = torch.inverse(tform).transpose(1,2).to(device)
                 original_image = testdata[i]['original_image'][None, ...].to(device)
                 _, orig_visdict = deca.decode(codedict, render_orig=True, original_image=original_image, tform=tform)    
-                orig_visdict['inputs'] = original_image            
+                orig_visdict['inputs'] = original_image
+                opdict['pose'] = codedict['pose']
 
         if args.saveDepth or args.saveKpt or args.saveObj or args.saveMat or args.saveImages:
             os.makedirs(os.path.join(savefolder, name), exist_ok=True)
